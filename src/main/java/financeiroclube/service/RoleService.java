239d5dd2b464a15f9ab4c0a9e237a9d70479bed3
adrianotads2 <altairmt@gmail.com>
2023-08-10 21:59:17 -0400

    Classe RoleService interfaceada

--- src/main/java/financeiroclube/service/RoleService.java
+++ src/main/java/financeiroclube/service/RoleService.java
@@ -1,17 +1,6 @@
 package financeiroclube.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import financeiroclube.repository.RoleRepository;
-
-@Service
-public class RoleService {
-
-	@Autowired
-	RoleRepository roleRepository;
-
-	public boolean roleExist(String role) {
-		return roleRepository.existsById(role);
-	}
+public interface RoleService {
+	public boolean roleExist(String role);
 }
