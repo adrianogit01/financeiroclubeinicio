c19920d20bdad96977cda08189b36b78f09537a0
AdrianoTads2 <adrianodesps2@outlook.com>
2024-12-07 20:37:20 -0800

    database changes

--- src/main/resources/semifnafusa.sql
+++
@@ -1,8 +0,0 @@
-use semifnafusa;
-
-CREATE TABLE IF NOT EXISTS `semifnafusa`.`roles` (
-  `nome_role` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`nome_role`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
